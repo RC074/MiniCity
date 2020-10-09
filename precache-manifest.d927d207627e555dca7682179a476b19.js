519df964c6ac55685693ad7c9559a8b997d93d4f
binarflow <clouds.overflow@gmail.com>
2020-10-09 19:40:13 -0400

    Updates

---
+++ precache-manifest.d927d207627e555dca7682179a476b19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d45af4771b116892573232e4b3be15",
+    "url": "/react-summative-app/index.html"
+  },
+  {
+    "revision": "06cf3182356dc30dc3f7",
+    "url": "/react-summative-app/static/css/main.54601cef.chunk.css"
+  },
+  {
+    "revision": "460ae3713e3e7ae2b7b7",
+    "url": "/react-summative-app/static/js/2.b2984be1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-summative-app/static/js/2.b2984be1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06cf3182356dc30dc3f7",
+    "url": "/react-summative-app/static/js/main.096262d5.chunk.js"
+  },
+  {
+    "revision": "5db92f955b000dddd0c1",
+    "url": "/react-summative-app/static/js/runtime-main.791149d7.js"
+  }
+]);
